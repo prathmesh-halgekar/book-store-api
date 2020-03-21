ef6ecfab6cdc2438344065c973d4dab38f581578
prats_apr25@yahoo.com <prats_apr25@yahoo.com>
2020-03-21 12:26:14 +0100

    refactor package names

--- demo/src/test/java/com/prats/bookstore/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/prats/bookstore/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kredit24.bookstore.demo;
+package com.prats.bookstore.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- demo/src/test/java/com/prats/bookstore/demo/service/UtilityServiceTest.java
+++ demo/src/test/java/com/prats/bookstore/demo/service/UtilityServiceTest.java
@@ -1,4 +1,4 @@
-package com.kredit24.bookstore.demo.service;
+package com.prats.bookstore.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
